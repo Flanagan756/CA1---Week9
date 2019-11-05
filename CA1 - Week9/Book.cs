d29c69f21be74f302602df2e4ea0911021d290d9
Harry Flanagan <S00189591@mail.itsligo.ie>
2019-11-05 16:22:29 +0000

    Add project files.

---
+++ CA1 - Week9/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1___Week9
+{
+    //enum
+    enum Genre
+    {
+        Fiction,
+        Biography,
+        Computing,
+        Literature
+    }
+    class Book: IComparable
+    {
+        //Properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public int Pages { get; set; }
+
+        public Genre Genre { get; set; }
+
+        //Constructor
+        public Book(string title, string author, string published, int pages, Genre genre)
+        {
+            //sets properties within default constructor
+            Title = title;
+            Author = author;
+            Published = Convert.ToDateTime(published);
+            Pages = pages;
+            Genre = genre;
+        }
+
+        //Message formate overide to the string
+        public override string ToString()
+        {
+
+            return (Author + "     " + Title + "     " + Pages + "     " + Genre + "     " + Published);
+        }
+
+        //Compares the authors within books
+        public int CompareTo(object obj)
+        {
+            Book that = (Book)obj;
+
+            return this.Author.CompareTo(that.Author);
+        }
+
+
+
+        }
+    }
---
+++ CA1 - Week9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1___Week9
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Created Objectes
+            Book bk1 = new Book("Ulysses", "James Joyce", "1922,02,02", 730, Genre.Literature);
+            Book bk2 = new Book("The C Programming Language", "B Kernighan and D Ritchie", "1978,02,28", 279, Genre.Computing);
+            Book bk3 = new Book("Steve Jobs", "Walter Isaacson", "1978/10/24", 627, Genre.Biography);
+            Book bk4 = new Book("The Hobbit", "JRR Tolkien", "1937/09/21", 310, Genre.Fiction);
+            Book bk5 = new Book("Portrait of the Artist", "James Joyce", "1916,12,29", 299, Genre.Literature);
+
+            //List
+            List<Book> ReadingList = new List<Book>();
+            //adds each book to the list
+            ReadingList.Add(bk1);
+            ReadingList.Add(bk2);
+            ReadingList.Add(bk3);
+            ReadingList.Add(bk4);
+            ReadingList.Add(bk5);
+
+            //Putting Books into an Array
+            Book[] allBooks = new Book[5];
+            allBooks[0] = bk1;
+            allBooks[1] = bk2;
+            allBooks[2] = bk3;
+            allBooks[3] = bk4;
+            allBooks[4] = bk5;
+
+
+            Console.WriteLine("Author         Title       Pages      Genre      Publication Date");
+
+            //Display Books
+            Display(ReadingList);
+            Console.WriteLine();
+
+            //sort by author
+            Array.Sort(allBooks);
+            Console.WriteLine("Sorting the Reading list by Author");
+
+            //DisplayBooks
+            Display(ReadingList);
+
+        }
+        //Method to diplay the list of books
+        private static void Display(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine("{0,-10}", book);
+            }
+        }
+
+     
+
+    }
+}
